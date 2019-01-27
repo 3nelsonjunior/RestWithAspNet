3e289505d1315025935c56a9e07b817f40037391
Nelson Junior <3nelsonjunior@gmail.com>
2019-01-27 20:45:10 -0200

    Implemented CalculatorController and endpoint sum

---
+++ RestWithAspNet_Calculator/RestWithAspNet_Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNet_Calculator.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+        
+        // GET api/values/5
+        [HttpGet("{number1}/{number2}")]
+        public IActionResult Sum(string number1, string number2)
+        {
+            if(IsNumber(number1) && IsNumber(number2))
+            {
+                var sum = ConvertToDecimal(number1) + ConvertToDecimal(number2);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumber(string strNumber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+
+            return isNumber;
+        }
+    }
+}
