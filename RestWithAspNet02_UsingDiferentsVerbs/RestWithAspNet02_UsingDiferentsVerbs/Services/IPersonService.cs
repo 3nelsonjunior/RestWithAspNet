0f4e92fa3fbb7101b3838f96500548df3bc216ae
Nelson Junior <3nelsonjunior@gmail.com>
2019-01-31 23:26:51 -0200

    Implementing Services - IPersonService

---
+++ RestWithAspNet02_UsingDiferentsVerbs/RestWithAspNet02_UsingDiferentsVerbs/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using RestWithAspNet02_UsingDiferentsVerbs.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet02_UsingDiferentsVerbs.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long person);
+        List<Person> FindAll();
+        Person Update(long person);
+        void Delete(long id);
+
+    }
+}
