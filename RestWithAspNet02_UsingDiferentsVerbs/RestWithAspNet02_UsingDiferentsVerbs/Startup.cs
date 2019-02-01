d6d89f602ee31a076fd248ca3bbf7dc60b5931df
Nelson Junior <3nelsonjunior@gmail.com>
2019-02-01 19:54:14 -0200

    71 - Connection with BD - Part 2

--- RestWithAspNet02_UsingDiferentsVerbs/RestWithAspNet02_UsingDiferentsVerbs/Startup.cs
+++ RestWithAspNet02_UsingDiferentsVerbs/RestWithAspNet02_UsingDiferentsVerbs/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RestWithAspNet02_UsingDiferentsVerbs.Services;
@@ -16,15 +17,18 @@ namespace RestWithAspNet02_UsingDiferentsVerbs
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+
+            services.AddDbContext<Model.Context.MySQLContext>(options => options.UseMySql(connection));
+
+            // add framework services
             services.AddMvc();
             // Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
